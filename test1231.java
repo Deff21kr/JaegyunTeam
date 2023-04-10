215c77b972864a4492a1a37d95923451b94164b3
Deff21kr <deff21kr@gmail.com>
2023-04-10 10:10:02 +0900

    jg first commit

---
+++ test1231.java
@@ -0,0 +1,15 @@
+class A{
+	//https://github.com/Deff21kr/JaegyunTeam/blob/main/test1231.java
+	public static void main(String args []){
+		String name1 = "HD";
+		System.out.printf("name : %s",name);
+		
+		String name2 = "DS";
+		String StringArray[] = new String [] { name1,name2, "HJ","KH"};
+		
+		for( String str : StringArray){
+			System.out.println(str);
+		}
+	}
+} 
+	
